685b8438dddfe5c394abde032619caf398291dc3
Rutger Broerze <contact@ruer.nl>
2025-08-18 16:31:29 +0200

    wip

--- resources/views/components/mobile-navigation.blade.php
+++ resources/views/components/mobile-navigation.blade.php
@@ -1,5 +1,5 @@
-<div x-cloak x-show="mobileNavOpen"
-     x-transition.opacity
+<div x-cloak
+     x-show="mobileNavOpen"
      @keydown.escape.window="mobileNavOpen = false"
      class="fixed inset-0 top-14 z-50 lg:hidden" role="dialog"
      tabindex="-1"
@@ -9,15 +9,18 @@
     <!-- backdrop -->
     <div class="fixed inset-0 top-14 bg-zinc-400/20 backdrop-blur-xs dark:bg-black/40"
          aria-hidden="true"
-         @click="mobileNavOpen = false"></div>
+         @click="mobileNavOpen = false"
+         x-show="mobileNavOpen"
+    ></div>
 
     <!-- panel -->
     <div>
         <div class="fixed top-14 bottom-0 left-0 w-full overflow-y-auto bg-white px-4 pt-6 pb-4 shadow-lg ring-1 shadow-zinc-900/10 ring-zinc-900/7.5 min-[416px]:max-w-sm sm:px-6 sm:pb-10 dark:bg-zinc-900 dark:ring-zinc-800"
-             x-transition:enter="transform transition ease-out duration-200"
+             x-show="mobileNavOpen"
+             x-transition:enter="transform transition ease-out duration-100"
              x-transition:enter-start="-translate-x-full"
              x-transition:enter-end="translate-x-0"
-             x-transition:leave="transform transition ease-in duration-150"
+             x-transition:leave="transform transition ease-in duration-75"
              x-transition:leave-start="translate-x-0"
              x-transition:leave-end="-translate-x-full">
             <nav>
